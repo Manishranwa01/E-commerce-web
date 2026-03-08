63c1016cffc45fd89a8f38fde29a07eca80decbd
khushwinder <khushwindersanhla02gamil.com>
2026-03-08 20:25:19 +0530

    QuestionAdd

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,22 @@
 import './App.css'
 import Footer from './compantes/footer/Footer';
 import Header from './compantes/header/Header'
+import Mulitivendor from './pages/products/Mulit-vendor/Mulitvendor'
+
 
 function App() {
 
   return (
-    
+    <>
+  
+      <Mulitivendor/>
+      {/* <Header/> */}
+     
+      <Footer/>
+
+      
+      
+    </>
   )
 }
 
